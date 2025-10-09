22e31a7da3a08c6165012e321c2f97b177d94a0d
김명식 <ms.kim@weddingbook.com>
2025-10-09 12:36:06 +0900

    feat: 사이드바 UI 추가

--- src/app/components/headers/Header.tsx
+++ src/app/components/headers/Header.tsx
@@ -1,24 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./header.module.scss";
 
 import LineIcon from "@/app/assets/images/headers/3line.png";
 import LogoIcon from "@/app/assets/images/headers/logo.png";
 import MartIcon from "@/app/assets/images/headers/mart.png";
 import AccountIcon from "@/app/assets/images/headers/account.png";
+import SideBar from "../sidebar/SideBar";
 
 export default function Header() {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
   return (
     <header className={styles.header}>
       <div className={styles["three-line-icon-container"]}>
-        <img src={LineIcon.src} alt="햄버거 아이콘" className={styles["three-line-icon"]} />
+        <img
+          src={LineIcon.src}
+          alt="햄버거 아이콘"
+          className={styles["three-line-icon"]}
+          onClick={() => {
+            setIsSideBarOpen(true);
+          }}
+        />
       </div>
       <img src={LogoIcon.src} alt="로고" className={styles["logo-icon"]} />
       <div>
         <img src={MartIcon.src} alt="마트" className={styles["mart-icon"]} />
         <img src={AccountIcon.src} alt="계정" className={styles["account-icon"]} />
       </div>
+      <SideBar closeSideBar={() => setIsSideBarOpen(false)} isSideBarOpen={isSideBarOpen} />
     </header>
   );
 }
---
+++ src/app/components/sidebar/SideBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React from "react";
+import styles from "./sidebar.module.scss";
+import CloseIcon from "./closeIcon.png";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  isSideBarOpen: boolean;
+  closeSideBar: () => void;
+};
+export default function SideBar({ isSideBarOpen, closeSideBar }: Props) {
+  const router = useRouter();
+  return (
+    <>
+      {isSideBarOpen && (
+        <section className={styles["sidebar"]}>
+          <div className={styles["sidebar-content"]}>
+            <div className={styles["sidebar-header"]}>
+              <img src={CloseIcon.src} alt="닫기" onClick={closeSideBar} className={styles["close-icon"]} />
+            </div>
+            <div className={styles["menu-wrap"]}>
+              <p
+                onClick={() => {
+                  router.push("/");
+                  closeSideBar();
+                }}
+              >
+                금시세(홈)
+              </p>
+              <p>상품판매</p>
+              <p>상품구입</p>
+              <p>좋아요</p>
+              <p>장바구니</p>
+              <p>마이페이지</p>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
+  );
+}
